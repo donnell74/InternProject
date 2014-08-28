1f4727616d6c4b32fa1c9e0154118607921e9d8e
donnell74 <donnell74@live.missouristate.com>
2014-08-28 04:41:30 -0500

    Problem 1: Added code to handle monthly invoices, policy 3 can be used to test the new code.  Also changed delete section of make_invoices (line 97)

--- accounting/tools.py
+++ accounting/tools.py
@@ -94,7 +94,7 @@ class PolicyAccounting(object):
 
     def make_invoices(self):
         for invoice in self.policy.invoices:
-            invoice.delete()
+            db.session.delete(invoice)
 
         billing_schedules = {'Annual': None, 'Semi-Annual': 3, 'Quarterly': 4, 'Monthly': 12}
 
@@ -131,7 +131,16 @@ class PolicyAccounting(object):
                                   self.policy.annual_premium / billing_schedules.get(self.policy.billing_schedule))
                 invoices.append(invoice)
         elif self.policy.billing_schedule == "Monthly":
-            pass
+            first_invoice.amount_due = first_invoice.amount_due / billing_schedules.get(self.policy.billing_schedule)
+            for i in range(1, billing_schedules.get(self.policy.billing_schedule)):
+                months_after_eff_date = i
+                bill_date = self.policy.effective_date + relativedelta(months=months_after_eff_date)
+                invoice = Invoice(self.policy.id,
+                                  bill_date,
+                                  bill_date + relativedelta(months=1),
+                                  bill_date + relativedelta(months=1, days=14),
+                                  self.policy.annual_premium / billing_schedules.get(self.policy.billing_schedule))
+                invoices.append(invoice)
         else:
             print "You have chosen a bad billing schedule."
 
